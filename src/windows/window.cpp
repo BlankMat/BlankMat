e75c3a12c132b276f0637e68839083aad436c63f
eestradaCS <eestrada@ufl.edu>
2023-10-05 16:12:19 -0400

    changed window.cpp to change opengl to core profile

--- src/windows/window.cpp
+++ src/windows/window.cpp
@@ -16,7 +16,7 @@ Window::Window(int width, int height, std::string name)
     glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
