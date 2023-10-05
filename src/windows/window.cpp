3fda6fd045062208b780d5718c4d867df3e583f1
MCKoleman <manu25025@gmail.com>
2023-10-05 16:08:21 -0400

    Swapped OpenGL to compatibility mode

--- src/windows/window.cpp
+++ src/windows/window.cpp
@@ -14,9 +14,9 @@ Window::Window(int width, int height, std::string name)
     // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
